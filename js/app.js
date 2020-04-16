28b8f8ee9aa06ae3c3a3eee2cb3fa7284fb389df
Christopher Brown <mrlordvondoombraun@gmail.com>
2020-04-16 14:48:43 -0700

    First Draft

--- js/app.js
+++ js/app.js
@@ -15,24 +15,57 @@
 
 /**
  * Define Global Variables
- * 
+ * var sec holds the NodeList for sections.
+ * var secs is number of sections. Use to programatically generate id.
+ * var dat holds dataset array.
 */
-
-
+const navList = document.getElementById("navbar__list");
+const secs = document.querySelectorAll("section");
+const navMenu = document.querySelector(".navbar__menu");
+let coords;
+let coordsX;
+let coordsY;
+   
 /**
  * End Global Variables
  * Start Helper Functions
  * 
-*/
-
-
-
+ */
+function getCoords(el) {
+     	coords = el.getBoundingClientRect();
+	coordsX = coords.left;
+	coordsY = coords.top;
+    }
 /**
  * End Helper Functions
  * Begin Main Functions
  * 
-*/
+ */
+function addItems() {
+    for(let i = 0; i < secs.length; i++) {
+	const list = document.createElement("li");
+	const anchor = document.createElement("a");
+	let item = secs[i].dataset.nav;
+	let refs = secs[i].getAttributeNode("id").value;
+	let attRef = document.createAttribute("href");
+	anchor.textContent = item;
+	attRef.textContent = "#" + refs;
+	anchor.setAttributeNode(attRef);
+	list.appendChild(anchor);
+	navList.appendChild(list);
+    }
+}
 
+function scrollIt(e) {
+    let whoDid = e.currentTarget;
+    whoDid.className = "active";
+    getCoords(e);
+    const whereY = coordsY + window.pageYOffset;
+    const whereX = coordsX + window.pageXOffset;
+    window.scrollTo(
+	{top: whereY, behavior: "smooth"}
+    )
+}
 // build the nav
 
 
@@ -48,8 +81,14 @@
  * 
 */
 
-// Build menu 
+document.addEventListener('DOMContentLoaded', addItems); 
+let anchors = document.querySelectorAll('a');
+anchors.forEach(addEvent);
 
+function addEvent() {
+    anchors.addEventListener('click', scrollIt);
+}
+// Build menu 
 // Scroll to section on link click
 
 // Set sections as active
