0063e8228288a3f1dbfbc55d1b43bfa8877ef931
Christopher Brown <mrlordvondoombraun@gmail.com>
2020-04-17 13:41:56 -0700

    Submittal Draft

--- js/app.js
+++ js/app.js
@@ -19,16 +19,22 @@
  * var secs is number of sections. Use to programatically generate id.
  * var dat holds dataset array.
 */
+
 const navList = document.getElementById("navbar__list");
 const secs = document.querySelectorAll("section");
 const navMenu = document.querySelector(".navbar__menu");
 let anchors = document.querySelectorAll("a");
+const heady = document.querySelector(".page__header");
+const footy = document.querySelector(".page_footer");
+const toppy = document.querySelector(".top");
 
 /**
  * End Global Variables
  * Start Helper Functions
  * 
  */
+
+
 
 /**
  * End Helper Functions
@@ -53,19 +59,28 @@ function addItems() {
 function makeActive() {
     for(let i = 0; i < secs.length; i++) {
 	window.addEventListener("scroll", function(){
+	    heady.className = "page__header";
 	    let whereDat = secs[i].getBoundingClientRect();
 	    if (whereDat.top + 64 > 0) {
 		secs[i].className = "act";
+		toppy.classList.remove("noshow");
+		toppy.classList.add("show");
 	    } else {
-		secs[i].className= "";
+		secs[i].className = "";
+		toppy.classList.remove("show");
+		toppy.classList.add("noshow");
 	    }
+	    setTimeout(function(){
+		heady.className = "page__header bye";
+		toppy.classList.remove("show");
+		toppy.classList.add("noshow");
+	    }, 3000);
 	}
-				);
+			       );	    
     }
 }
-// build the nav
 
-
+// build the nav	
 // Add class 'active' to section when near top of viewport
 
 
@@ -80,6 +95,10 @@ function makeActive() {
 
 document.addEventListener('DOMContentLoaded', addItems);
 document.addEventListener('DOMContentLoaded', makeActive);
+document.addEventListener('mousemove', function(){
+    heady.className="page__header";
+});
+			  
 // Build menu 
 // Scroll to section on link click
 
